a27fac2aebca2540cbfa79f108af54694e5b6451
Lokesh-Bharath <bharathchandra.putnam@gmail.com>
2024-05-06 20:24:11 -0400

    Fixed test cases and adressed comments

--- test/quizzes_test.py
+++ test/quizzes_test.py
@@ -3,11 +3,18 @@ import datetime
 
 from app.controllers.quizzes_controller import QuizzesController
 
+# Creating a custom exception to handle database errors. Developers can include more information
+class DataBaseError(Exception):
+    pass
+
 class QuizzesTest(unittest.TestCase):
 
     def setUp(self):
         # Run tests on non-production data
         self.ctrl = QuizzesController('quizzes_test.py')
+        
+    def tearDown(self) -> None:
+        self.ctrl.clear_data()
         
     def test_expose_failure_01(self):
         """
@@ -17,13 +24,13 @@ class QuizzesTest(unittest.TestCase):
         be concatenated with a string which is not possible.
         """
 
-        with self.assertRaises(Exception):
+        with self.assertRaises(AttributeError):
             self.ctrl.add_quiz(-1, "Welcome to the quiz 1", datetime.datetime(2024, 5, 6), datetime.datetime(2024, 5, 17))
         
         """
         Using try and except block to catch the exception raised by the add_quiz method when
-        the title is not a string can handle this failure. In that case the test case will pass 
-        without the system crashing.
+        the title is not a string can handle this failure. Recommendation is to gracefully return another error.
+        In that case the test case will pass without the system crashing.
         """
         
 
@@ -31,48 +38,46 @@ class QuizzesTest(unittest.TestCase):
         """
         The following test case is designed to expose a failure in add_question method.
         The method is expected to raise an exception when the quiz title is not a string.
-        But instead it fails at Line 81 where it is saving the data to the JSON file.
-        The error roots at Line 78 where date time object is used in an fstring which is not possible.
+        But instead it fails at Line 81 of quizzes_controller.py where it is saving the data to the JSON file.
+        The error roots at Line 78 of quizzes_controller.py where date time object is used in an fstring which is not possible.
+        The exact problem is trying to save non-json data to a json file at Line 21 in data_loader.py.
         This method halts while writing the data to the JSON file corrupting the file which is talked 
         about in the next test case.
         """
 
         new_quiz_id = self.ctrl.add_quiz("Software Engineering - Quiz 1", "Welcome to the quiz 1", datetime.datetime(2024, 5, 6), datetime.datetime(2024, 5, 17))
         randomDate = datetime.datetime(2024, 5, 17)
-        with self.assertRaises(Exception):
+        with self.assertRaises(AttributeError):
             self.ctrl.add_question(new_quiz_id, randomDate, "Why is SDLC important?")
         
         """
         Using try and except block to catch the exception raised by the add_question method when
-        the quiz title is not a string can handle this failure. In that case the test case will pass
-        without the system crashing.
-        """
-            
-        
+        the quiz title is not a string can handle this failure. Recommendation is to gracefully return another exception.
+        In that case the test case will passwithout the system crashing.
+        """  
 
     def test_expose_failure_03(self):
         """
-        The following test case is designed to expose a failure in loading and saving data.
-        Methods save_data and load_data are expected to raise an exception when the data is corrupted.
-        But instead they fail by trying to read a corrupted file and not being able to convert the data to json.
-        The code fais at Line 55 in _save_data method in quizzes_controller.py.
-        The error roots at Line 21 in data_loader.py where it is trying to convert the data to json.
+        This test case is designed to expose a failure in get_quiz_by_id and related getter methods.
+        These methods are expected to raise an exception when the data is corrupted in the JSON file.
+        But instead they try to load the corrupted data and fail at Line 27 in data_loader.py. This causes 
+        all the getter methods to fail as they are dependent on the data loaded from the JSON file.
         """
-        new_quiz_id = self.ctrl.add_quiz("Software Engineering", "Welcome to the quiz", datetime.datetime(2024, 5, 6), datetime.datetime(2024, 5, 17))
-        randomDate = datetime.datetime(2024, 5, 17)
 
-        #This test case was case 2 which is just used for corrupting the file
-        with self.assertRaises(Exception):
-            self.ctrl.add_question(new_quiz_id, randomDate, "Why is SDLC important?")
+        #Corrupting the data in the JSON file and loading it!
+        with open('data/quizzes_test.py', 'w') as file:
+            file.write('{"availaT00:00:00","id90": "0523456e293e3d46ceaa60e5a9b27653","last_updatT19:32:22.272055","see quiz 1",are Engineering - Quiz 1"}]')
+        self.ctrl._load_data()
         
-        #This is the new test case which is designed to expose the failure in get_quiz_by_id method.
-        #This extends to all other getter methods in the system.
-        with self.assertRaises(Exception):
-            new_quiz_id = self.ctrl.add_quiz("Software Engineering Quiz 2", "Welcome to the quiz 2", datetime.datetime(2024, 5, 6), datetime.datetime(2024, 5, 17))
-
+        #Trying to get the quiz by id, the code crashes. But the test case should pass without the
+        #system crashing. It can return a custom exception called DataBaseError.
+        with self.assertRaises(DataBaseError):
+            self.ctrl.get_quiz_by_id("0523456e293e3d46ceaa60e5a9b27653")
+            
         """
-        Using try and except block to catch the exception raised by the load_data and save_data methods when
-        the data is corrupted can handle this failure. In that case the test case will pass without the system crashing.
+        Using try and except block to catch the exception raised by the load_data method when
+        the data is corrupted can handle this failure. Recommendation is to gracefully return another exception.
+        In that case the test case will pass without the system crashing.
         """
 
 if __name__ == '__main__':
